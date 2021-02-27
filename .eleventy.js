663b3ccd3605841f53f26254285ce9b22c28982b
Devin Halladay <devin@devinhalladay.com>
2021-02-26 19:54:16 -0500

    Add markdown-it plugin to process href with _blank target

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,37 @@
 module.exports = function (eleventyConfig) {
+  let markdownIt = require("markdown-it");
+  let mila = require("markdown-it-link-attributes");
+
   eleventyConfig.addPassthroughCopy("assets/");
   eleventyConfig.addPassthroughCopy("CNAME");
-
 
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
   });
 
+  let markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };;
+
+  let milaOptions = {
+    pattern: /^(?!(https:\/\/hydraulics\.nyc|#)).*$/gm,
+    attrs: {
+      target: "_blank",
+      rel: "noopener noreferrer",
+    },
+  };
+
+  let markdownLib = markdownIt(markdownItOptions).use(mila, milaOptions);
+
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
       input: "views",
       includes: "elements",
-      layouts:"layouts"
+      layouts: "layouts",
     },
   };
 };
